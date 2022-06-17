aa79010f6df2e6b38b86e72b29c0b78386f10d35
assiduityk1m <assiduityk1m@gmail.com>
2022-06-17 15:13:19 +0900

    가운데 글자 가져오기

---
+++ Level1/Level1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Level1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Program.CenterValue("programmers"));
+        }
+        public static string CenterValue(string s)
+        {
+            string answer = "";
+
+            if (s.Length % 2 == 0 && s.Length != 0)
+            {
+
+                //answer += s[s.Length / 2  - 1];
+                //answer += s[s.Length / 2];
+
+                //answer = s[s.Length / 2 - 1].ToString() + s[s.Length / 2].ToString();
+
+                answer = Convert.ToString(s[s.Length / 2 - 1]) + Convert.ToString(s[s.Length / 2]);
+            }
+            else if (s.Length % 2 == 1)
+            {
+                answer += s[s.Length / 2];
+            }
+
+            return answer;
+        }
+    }
+}
