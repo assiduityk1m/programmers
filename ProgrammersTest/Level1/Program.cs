88e694008bece952fe4747a5568a5ba8dfb310d5
YoungHwan Kim <assiduityk1m@gmail.com>
2022-07-14 15:55:03 +0900

    update

--- ProgrammersTest/Level1/Program.cs
+++ ProgrammersTest/Level1/Program.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 
-
-
 namespace Level1
 {
     class Program
     {
         static void Main(string[] args)
         {
+
             //chapter1 - 가운데 글자 가져오기
-            chapter1.Chapter1();
+            //chapter1.Chapter1();
 
             //chapter2 - 로또의 최고 순위와 최저 순위
             chapter2.Chapter2();
 
-            //chapter 3 - 
+            //chapter3 - 직사각형 별찍기
+            
+            
 
         }
 
--- ProgrammersTest/Level1/chapter3.cs
+++ ProgrammersTest/Level1/chapter3.cs
@@ -4,7 +4,27 @@ using System.Text;
 
 namespace Level1
 {
-    class chapter3
-    {
+    //chapter3 - 직사각형 별찍기
+    public class chapter3
+    {      
+        public static void Chapter3()
+        {
+            String[] s;
+
+            Console.Clear();
+            s = Console.ReadLine().Split(' ');
+
+            int a = Int32.Parse(s[0]);
+            int b = Int32.Parse(s[1]);
+
+            for (int i = 0; i < b; i++)
+            {
+                for (int j = 0; j < a; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+        }
     }
 }
